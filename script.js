1eb02fa68b6af712387265ccf87cb7d3274c3634
Sothyra12 <schan256@my.centennialcollege.ca>
2025-02-27 22:31:28 -0500

    added existent code for styling & created an async() function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // script.js
+
+const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
+const forumTopicUrl = "https://forum.freecodecamp.org/t/";
+const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
+const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
+
+// To populate the forum leaderboard with data, you will need to request the data from an API. 
+// This is known as an asynchronous operation, which means that tasks execute independently of the main program flow.
+// You can use the async keyword to create an asynchronous function, which returns a promise.
+
+const fetchData = async () => {
+    try {
+        
+    } catch (err) {
+        
+    }
+};
